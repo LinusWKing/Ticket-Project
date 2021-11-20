f428b1705f5c60a9e97e721168ba6b9f7e242c0d
LinusWKing <linuswahomeking@gmail.com>
2021-11-20 10:51:54 +0300

    Suppress password echoing

--- main.cpp
+++ main.cpp
@@ -1,9 +1,42 @@
+
+#ifdef WIN32
+#include <windows.h>
+#else
+#include <termios.h>
+#include <unistd.h>
+#endif
+
 #include <iostream>
 #include <string>
 #include <conio.h>
 #include <stdio.h>
 #include <time.h>
 #include <vector>
+
+void SetStdinEcho(bool enable = true)
+{
+#ifdef WIN32
+    HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+    DWORD mode;
+    GetConsoleMode(hStdin, &mode);
+
+    if (!enable)
+        mode &= ~ENABLE_ECHO_INPUT;
+    else
+        mode |= ENABLE_ECHO_INPUT;
+
+    SetConsoleMode(hStdin, mode);
+#else
+    struct termios tty;
+    tcgetattr(STDIN_FILENO, &tty);
+    if (!enable)
+        tty.c_lflag &= ~ECHO;
+    else
+        tty.c_lflag |= ECHO;
+
+    (void)tcsetattr(STDIN_FILENO, TCSANOW, &tty);
+#endif
+}
 
 static int ticketCount = 0, empCount = 0; // Defined global variables
 int main();                               // Prototyping main()
@@ -396,7 +429,9 @@ void Employee ::login()
             std::cout << "\nLogin ID: ";
             std::cin >> loginID;
             std::cout << "\nPassword: ";
+            SetStdinEcho(false);
             std::cin >> password;
+            SetStdinEcho(true);
 
             for (size_t i = 0; i < 5; i++)
             {
